3358ae6d10017ff46034fd62f76d7c0f38930a9e
Saurabh-Shukla-G <107531009+Saurabh-Shukla-G@users.noreply.github.com>
2023-02-22 16:37:35 +0530

    Kuchh bhi

--- index.js
+++ index.js
@@ -8,44 +8,37 @@ document.addEventListener("keypress", eventListener);
 
 function eventListener(event)
 {   
-    if(event.key)
+    if(event.key.toLowerCase())
         temp=event.key;
     else
         temp=this.innerHTML;
     switch(temp)
     {
         case "w":
-        case "W":
-            var tom1= new Audio("sounds/tom-1.mp3");
+           var tom1= new Audio("sounds/tom-1.mp3");
             tom1.play();
             break;
         case "a":
-        case "A":
-            var tom2= new Audio("sounds/tom-2.mp3");
+           var tom2= new Audio("sounds/tom-2.mp3");
             tom2.play();
             break;
         case "s":
-        case "S":
             var tom3= new Audio("sounds/tom-3.mp3");
             tom3.play();
             break;
         case "d":
-        case "D":
             var tom4= new Audio("sounds/tom-4.mp3");
             tom4.play();
             break;
         case "j":
-        case "J":
             var crash= new Audio("sounds/crash.mp3");
             crash.play();
             break;
         case "k":
-        case "K":
             var kick= new Audio("sounds/kick-bass.mp3");
             kick.play();
             break;
         case "l":
-        case "L":
             var snare= new Audio("sounds/snare.mp3");
             snare.play();
             break;
